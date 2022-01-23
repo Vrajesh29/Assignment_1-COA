2a5c9da8c4067770316576ccc970eca31bea2942
Vrajesh-code <75999530+Vrajesh-code@users.noreply.github.com>
2022-01-23 14:58:42 +0530

    Question_2

---
+++ Q2_a_integer_clang.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    double time_spent=0.0;  
+    clock_t begin = clock();
+    int i,j,k,n;
+    printf("Enter n:");
+    scanf("%d", &n);
+    int m1[n][n];
+    int m2[n][n];
+    srand(time(0));
+    for(i = 0; i < n;i++) {
+    	for(j = 0; j < n;j++) {
+    	    m1[i][j]=rand();
+    	    m2[i][j]=rand();
+    	}
+    }
+    int res[n][n];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            res[i][j] = 0;
+            for (k = 0; k < n; k++)
+                res[i][j] += m1[i][k] * m2[k][j];
+        }
+    }
+    
+    printf("Matrix_1:");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", m1[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+    
+    printf("Matrix_2:");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", m2[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+    
+    printf("Result:");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d ", res[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+    clock_t end = clock();
+    time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("%f", time_spent);
+    return 0;
+}
