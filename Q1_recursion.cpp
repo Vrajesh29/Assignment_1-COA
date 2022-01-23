03e8392329b16cf00cc7f710bc649255534c11f6
Vrajesh-code <75999530+Vrajesh-code@users.noreply.github.com>
2022-01-23 12:46:11 +0530

    Question_1

--- Q1_recursion.cpp
+++ Q1_recursion.cpp
@@ -3,35 +3,6 @@
 
 using namespace std;
 
-std::ostream &
-operator<<(std::ostream &dest, __int128 value)
-{
-    std::ostream::sentry s(dest);
-    if (s)
-    {
-        __int128 tmp = value < 0 ? -value : value;
-        char buffer[256];
-        char *d = std::end(buffer);
-        do
-        {
-            --d;
-            *d = "0123456789"[tmp % 10];
-            tmp /= 10;
-        } while (tmp != 0);
-        if (value < 0)
-        {
-            --d;
-            *d = '-';
-        }
-        int len = std::end(buffer) - d;
-        if (dest.rdbuf()->sputn(d, len) != len)
-        {
-            dest.setstate(std::ios_base::badbit);
-        }
-    }
-    return dest;
-}
-
 __int128 fib(__int128 x)
 {
     if ((x == 1) || (x == 0))
@@ -63,3 +34,4 @@ int main()
     cout << "\nTime taken: " << elapsed << " seconds\n";
     return 0;
 }
+
